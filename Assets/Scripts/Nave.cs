2ffd33b50e51b7c2a0daec84e8ddb7fb62061c3a
PatAm16 <patricia.amaral16@gmail.com>
2021-04-13 21:09:01 +0100

    Update Nave.cs

--- Assets/Scripts/Nave.cs
+++ Assets/Scripts/Nave.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Nave : MonoBehaviour
 {
@@ -10,26 +12,62 @@ public class Nave : MonoBehaviour
     [SerializeField]
     float maxRotation = 10f;
 
+
+
     [SerializeField]
     float thrustForce = 150f;
 
     [SerializeField]
     float torqueForce = 25f;
 
+
+    [SerializeField]
+    float combustivel = 500f;
+
+    [SerializeField]
+    float forcaCombustivel = 10f;
+
+    [SerializeField]
+    float combustivelTorque = 5f;
+
+
+    [SerializeField]
+    TMP_Text txtfuelValue;
+
+
+
     private void Update()
     {
         if(Input.GetKey(KeyCode.UpArrow))
         {
+
+            if (combustivel > 0)
+            { 
+
             GetComponent<Rigidbody2D>().AddForce(thrustForce * transform.up * Time.deltaTime);
 
-        } else if(Input.GetKey(KeyCode.LeftArrow))
+            }
+
+        }
+        else if(Input.GetKey(KeyCode.LeftArrow))
         {
+            if (combustivel > 0)
+            { 
             GetComponent<Rigidbody2D>().AddTorque(-torqueForce * Time.deltaTime);
+                combustivel -= forcaCombustivel * Time.deltaTime;
+
+            }
+
 
         } else if ((Input.GetKey(KeyCode.RightArrow)))
         {
+
+            if (combustivel > 0)
             GetComponent<Rigidbody2D>().AddTorque(torqueForce * Time.deltaTime);
+            combustivel -= forcaCombustivel * Time.deltaTime;
         }
+
+        txtfuelValue.text = Mathf.RoundToInt(combustivel).ToString();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
